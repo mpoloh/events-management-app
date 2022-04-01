40368fab860de87b9137e0b197fa03eb7d259710
mpolo <manolo.polo.hoyos@gmail.com>
2022-04-01 05:30:29 +0200

    add ButtonAppBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './ButtonAppBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header >
+        <ButtonAppBar />
+        
+      </header>
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
