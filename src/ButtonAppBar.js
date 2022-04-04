715b6f6583257d8b67b456c9e3e7b98b1d20ea8c
mpolo <manolo.polo.hoyos@gmail.com>
2022-04-04 20:15:52 +0200

    add menu and delete button 'login'

--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -3,11 +3,20 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar, Divider, ListItemIcon, Menu, MenuItem } from '@mui/material';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 
 export default function ButtonAppBar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,13 +27,74 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+            elevation: 0,
+            sx: {
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                mt: 1.5,
+                '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+                },
+                '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                left: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+                },
+            },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+           >
+            <MenuItem>
+            <Avatar /> Profile
+            </MenuItem>
+            <MenuItem>
+            <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+            <ListItemIcon>
+                <PersonAdd fontSize="small" />
+            </ListItemIcon>
+            Add another account
+            </MenuItem>
+            <MenuItem>
+            <ListItemIcon>
+                <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+            </MenuItem>
+            <MenuItem>
+            <ListItemIcon>
+                <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+            </MenuItem>
+          </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Event management
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
